4a5c3e18ea67b8a36ce688f286f31226e5896dfc
Vlad satonin <vlad.satonin74@gmail.com>
2019-12-27 21:27:11 +0500

    input add

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 int main (void){
-   string hw = "Hello World";
+   string hw = "Hello World and ...";
+   string name;
+   cout<<"¬ведите свое им€: ";cin>>name;
    cout<<hw<<endl;
    return 0;
 }
