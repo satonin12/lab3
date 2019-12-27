70c784157dc0bda86d7143bd53d896875f42e047
Vlad satonin <vlad.satonin74@gmail.com>
2019-12-27 21:39:17 +0500

    adding 7 string

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main (void){
    string name;
    cout<<"Введите свое имя: ";cin>>name;
    if(typeid(name) == typeid(string)){
-      cout<<hw<<name<<endl;
+      cout<<hw<<name<<" 7 "<<endl;
    }else{
       cout<<"Неправильное тип переменной name";
    }
